04e0c84d24e6849ed6cb614d523785196baa1c18
github asaiayaka <ayaka.asai@jp.ricoh.com>
2025-04-18 12:40:48 +0900

    ch03ex01修正

--- exercises/ch03/ex01/index.js
+++ exercises/ch03/ex01/index.js
@@ -1,3 +1,18 @@
-console.log(Infinity + Nan);
-console.log(-Infinity + Nan);
-console.log(Infinity + Nan);
+console.log(Infinity + NaN);
+console.log(-Infinity + NaN);
+console.log(Infinity - NaN);
+console.log(-Infinity - NaN);
+console.log(Infinity * NaN);
+console.log(-Infinity * NaN);
+console.log(Infinity / NaN);
+console.log(-Infinity / NaN);
+
+// 結果
+// NaN
+// NaN
+// NaN
+// NaN
+// NaN
+// NaN
+// NaN
+// NaN
