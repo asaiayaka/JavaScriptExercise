6a0c1acb2e3142da0675cb25d33da9c6f2c62174
github asaiayaka <ayaka.asai@jp.ricoh.com>
2025-04-27 17:07:54 +0900

    ch05ex02途中

---
+++ exercises/ch05/ex01/index.js
@@ -0,0 +1,18 @@
+function messageFunction() {
+    {
+        const message = "Hello from block 1";
+        console.log(message);
+    }
+    
+    {
+        const message = "Hello from block 2";
+        console.log(message);
+    }
+
+    {
+        const message = "Hello from block 3";
+        console.log(message);
+    }
+}
+
+messageFunction();
---
+++ exercises/ch05/ex02/ifelse.js
@@ -0,0 +1,6 @@
+function escapeStringIfElse(str) {
+    let result = "";
+    for (i = 0; i < str.length; i++) {
+        let ch = str[i];
+    }
+}
